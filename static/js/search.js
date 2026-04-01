27617e5dfbea6fd5b50d691c072512a565e3b48a
JadedaJ-fo3o <wangjiayu_wjy@126.com>
2026-04-01 18:59:08 +0200

    maintain

--- static/js/search.js
+++ static/js/search.js
@@ -40,21 +40,23 @@ function searchShows(query) {
     });
 }
 
-document.getElementById("search-button").addEventListener("click", function (event) {
+document
+  .getElementById("search-button")
+  .addEventListener("click", function (event) {
     event.preventDefault();
     const query = document.getElementById("search-input").value.trim();
     if (!query) return;
     sessionStorage.setItem("histoire", query);
     searchShows(query);
+  });
+
+window.addEventListener("load", function () {
+  const lastSearch = document.getElementById("search-input").value.trim();
+  if (!lastSearch) return;
+  document.getElementById("search-input").value = lastSearch;
+  searchShows(lastSearch);
 });
 
-window.addEventListener('load', function() {
-    const lastSearch = document.getElementById("search-input").value.trim();
-    if (!lastSearch) return
-    document.getElementById('search-input').value = lastSearch
-    searchShows(lastSearch)
-})
-
 document.getElementById("home-page").addEventListener("click", function () {
-  window.location.href = "/home-test";
+  window.location.href = "/home";
 });
